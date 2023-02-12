34a33ae62fcac03d9c71b4d77d2fa93728b2bf42
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-02-12 20:46:34 +0100

    matrial lib --buildable --addTailwind
    nx g @nrwl/angular:library material --buildable --addTailwind

--- portfolio/libs/material/src/lib/material.module.ts
+++ portfolio/libs/material/src/lib/material.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavigationComponent} from "./navigation/navigation.component";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {RouterOutlet} from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule, RouterOutlet],
-  declarations: [NavigationComponent],
-  exports: [NavigationComponent]
+  imports: [CommonModule],
 })
 export class MaterialModule {}

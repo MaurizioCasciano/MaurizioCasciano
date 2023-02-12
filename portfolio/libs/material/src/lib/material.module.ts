80ac0fea632cade41c0a93bf988d399d55e0096f
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-02-12 18:35:56 +0100

    navigation component declaration & imports

--- portfolio/libs/material/src/lib/material.module.ts
+++ portfolio/libs/material/src/lib/material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NavigationComponent} from "./navigation/navigation.component";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent]
 })
 export class MaterialModule {}

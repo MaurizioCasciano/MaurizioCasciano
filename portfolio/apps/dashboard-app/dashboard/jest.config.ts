853495274820f58493e059aaee7c29b99ef0e2cd
Maurizio Casciano <mauriziocasciano7@gmail.com>
2023-02-12 16:51:34 +0100

    @nrwl/angular:move
    nx g @nrwl/angular:move --project dashboard --destination dashboard-app/dashboard
    nx g @nrwl/angular:move --project dashboard-e2e --destination dashboard-app/dashboard-e2e

--- portfolio/apps/dashboard-app/dashboard/jest.config.ts
+++ portfolio/apps/dashboard-app/dashboard/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'dashboard',
-  preset: '../../jest.preset.js',
+  displayName: 'dashboard-app-dashboard',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/dashboard',
+  coverageDirectory: '../../../coverage/apps/dashboard-app/dashboard',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
